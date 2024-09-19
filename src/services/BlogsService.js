82aef6a358782be2cab10a44c727b32f63847f19
Autumn Eckley <eckley.autumn@gmail.com>
2024-09-19 14:33:01 -0600

    Got blogs in console

---
+++ src/services/BlogsService.js
@@ -0,0 +1,13 @@
+import { errorMessages } from "vue/compiler-sfc"
+import { api } from "./AxiosService.js"
+import { logger } from "@/utils/Logger.js"
+
+class BlogsService {
+  async getAllBlogs() {
+    const response = await api.get('api/blogs')
+    logger.log('Got blogs - blogsService', response.data)
+  }
+
+}
+
+export const blogsService = new BlogsService
